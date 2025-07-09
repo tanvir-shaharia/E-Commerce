bc3eb1015b03f22ebcc746cee94a93b9502a02be
tanvir-shaharia <tanvirking29@gmail.com>
2025-07-09 18:23:53 +0600

    add tailwindcss and Catalog page design

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,20 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "product price must be positive" do
+    product = Product.new(title: "My Book Title",
+                          description: "yyy",
+                          image_url: "zzz.pdf")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+                 product.errors[:price]
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+                 product.errors[:price]
+    product.price = 1
+    assert product.valid?
+  end
+
 end
